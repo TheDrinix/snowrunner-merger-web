4033de5227f81bc5947a300b5185b9d2d5ddb2f6
Drinix <thedrinix@gmail.com>
2024-10-22 11:05:23 +0200

    Fix footer positioning

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,15 @@ import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-  <Header />
-  <div class="container mx-auto mt-4">
-    <main class="pb-3">
-      <RouterView />
-    </main>
+  <div class="relative min-h-screen pb-16">
+    <Header />
+    <div class="container mx-auto mt-4">
+      <main class="pb-3">
+        <RouterView />
+      </main>
+    </div>
+    <Footer />
   </div>
-  <Footer />
 </template>
 
 <style scoped>
